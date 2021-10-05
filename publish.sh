dfa2138061cbc89356b56d805dd7823e4432970f
Xmo <18851989097@163.com>
2021-10-06 01:46:14 +0800

    修改publish.sh

--- publish.sh
+++ publish.sh
@@ -3,10 +3,12 @@ set -e
 
 # echo '登录'
 # npm login 
+npm config ser registry https://registry.npmjs.org
 
 echo "发布中..."
 npm publish
 
+npm config set registry https://registry.npmmirror.com
 
 echo -e "\n发布成功\n"
 exit
